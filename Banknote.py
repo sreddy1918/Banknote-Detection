6ae0f376fefb4403b23379d7148ba11cc5c479f7
sreddy1918 <164194950+sreddy1918@users.noreply.github.com>
2024-04-30 12:05:08 -0400

    Update Banknote.py

--- Banknote.py
+++ Banknote.py
@@ -51,6 +51,6 @@ async def predict_banknote(data:BankNote):
 # 5. Run the API with uvicorn
 #    Will run on http://127.0.0.1:8000
 if __name__ == '__main__':
-     uvicorn.run(app, host='127.0.0.1', port=8000)
+     uvicorn.run(app)
     
 #uvicorn untitled0:app --reload
