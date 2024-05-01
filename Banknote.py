2eb1295135d2d5a82917e3526836b771772260ff
sreddy1918 <164194950+sreddy1918@users.noreply.github.com>
2024-05-01 13:44:56 -0400

    Add files via upload

--- Banknote.py
+++ Banknote.py
@@ -51,6 +51,11 @@ async def predict_banknote(data:BankNote):
 # 5. Run the API with uvicorn
 #    Will run on http://127.0.0.1:8000
 if __name__ == '__main__':
-     uvicorn.run(app)
+     uvicorn.run(app, host='127.0.0.1', port=8000)
     
 #uvicorn untitled0:app --reload
+
+# {'variance': ,
+#'skewness':,
+#'curtosis':,
+#'entropy':}
